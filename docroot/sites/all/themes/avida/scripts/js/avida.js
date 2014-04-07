103068c67fadeab86421a7259da84bde2edc8df0
Tom Beigle <tbeigle@gmail.com>
2014-04-07 12:22:29 -0500

    Added JS mobile menu scripts.

--- docroot/sites/all/themes/avida/scripts/js/avida.js
+++ docroot/sites/all/themes/avida/scripts/js/avida.js
@@ -1,6 +1,8 @@
 (function($) {
+  // Common JS
   Drupal.behaviors.avida = {
     attach: function (context, settings) {
+      // Vertical scrolling links
       $('a[href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -12,6 +14,27 @@
             return false;
           }
         }
+      });
+    }
+  }
+  
+  // Mobile JS
+  Drupal.behaviors.avidaMobile = {
+    attach: function(context, settings) {
+      var dur_mobile_expand = 200,
+          class_mobile_expanded = 'mobile-expanded';
+      
+      // Mobile navigation
+      $('.mobile-title.block-menu').each(function() {
+        var $this = $(this),
+            $title = $this.find('.block-title'),
+            $menu = $this.find('.content > ul.menu');
+        
+        $title.click(function() {
+          $menu.slideToggle(dur_mobile_expand);
+          $this.toggleClass(class_mobile_expanded);
+          return false;
+        });
       });
     }
   }
